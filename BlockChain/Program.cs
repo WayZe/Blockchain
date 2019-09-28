d983ee7bf2c2704b411999e06e5f6622486bee72
WayZe <wayze13@yandex.ru>
2019-09-28 17:38:00 +0300

    feat: Add Linq.

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -1,37 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace Blockchain
 {
     class Program
     {
-        private const int N = 0;
-        private static int[] arr;
-
         static void Main(string[] args)
         {
-            arr = new int[] { 25, 14, 32, 40, 8, 23, 2, 7 };
+            var arr = new int[] { 25, 14, 32, 40, 8, 23, 2, 7 };
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr.Length - 1 - i; j++)
-                {
-                    if (arr[j] < arr[j + 1])
-                    {
-                        int tmpParam = arr[j];
-
-                        arr[j] = arr[j + 1];
-
-                        arr[j + 1] = tmpParam;
-                    }
-                }
-            }
+            arr = arr.Where(x => x > 14).OrderByDescending(x => x).ToArray();
 
             foreach (int x in arr)
-            {
-                if (x > 14)
-                {
-                    Console.WriteLine(x);
-                }
+            { 
+                Console.WriteLine(x);
             }
         }
     }
